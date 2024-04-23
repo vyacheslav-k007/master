fba8e9a56cbebeff757a849a350c66dc25fe3883
Your Name <you@example.com>
2024-04-23 09:51:31 +0300

    Added an update to Lesson 7

--- src/lesson7/Circles/Main.java
+++
@@ -1,9 +0,0 @@
-package lesson7.Circles;
-
-public class Main {
-    public static void main(String[] args) {
-        Circle circle1 = new CircleNew(5);
-        circle1.diameterCalculation();
-        }
-    }
-
